4a1732c6c5115213368424afa7613cff42261cb0
Sebastian Zecenarro <sebazecenarro@gmail.com>
2018-05-11 18:10:00 -0300

    primera_integracion_bootstrap_4

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <head>
     <title>Aventón</title>
     <meta charset= "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="/css/index.css" rel="stylesheet" type="text/css">
   </head>
   <body>
@@ -35,7 +37,7 @@
           <input type="email" name="mail">
           <br>
           <h3>Ingrese su fecha de nacimiento:</h3>
-          Día: 
+          Día:
           <select name="birth_day">
             <?php
               for($i = 1; $i <= 31; $i++){
@@ -74,4 +76,7 @@
       <a id="help_link"  href="/php/ayuda.php">Ayuda y Contacto</a>
     </div>
   </body>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
