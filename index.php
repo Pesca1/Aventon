0ff03ee180765c0f7ed56ef06dcc2acfc2c979c3
Joaquín de Antueno <joaquindea@hotmail.es>
2018-05-12 19:43:47 -0300

    Progreso en registrar_usuario.php

--- index.php
+++ index.php
@@ -8,73 +8,75 @@
     <link href="/css/index.css" rel="stylesheet" type="text/css">
   </head>
   <body>
-    <div id="header">
-      <h2>Aventón</h2>
-    </div>
-    <div id="body">
-      <h1>Bienvenido a Aventón!</h1>
-      <div class="form_container">
-        <h2>Iniciar sesión</h2>
-        <form>
-          <h3>Ingrese su mail:</h3>
-          <input type="email" name="mail">
-          <br>
-          <h3>Ingrese su contraseña:</h3>
-          <input type="password" name="password">
-          <br><br>
-          <input type="submit" text="Iniciar Sesión">
-          <br><br>
-          <a href="/php/recuperar_contraseña.php">Olvide mi contraseña!</a>
-        </form>
+    <div class="container-fluid">
+      <div id="header">
+        <h2>Aventón</h2>
       </div>
-      <div class="form_container">
-        <h2>Crear una cuenta</h2>
-        <form action="/php/registrar_usuario.php" method="post">
-          <h3>Ingrese su nombre completo:</h3>
-          <input type="text" name="name">
-          <br>
-          <h3>Ingrese su mail:</h3>
-          <input type="email" name="mail">
-          <br>
-          <h3>Ingrese su fecha de nacimiento:</h3>
-          Día:
-          <select name="birth_day">
-            <?php
-              for($i = 1; $i <= 31; $i++){
-                echo "<option>$i</option>";
-              }
-              ?>
-          </select>
-           - Mes:
-          <select name="birth_month">
-            <?php
-              for($i = 1; $i <= 12; $i++){
-                echo "<option>$i</option>";
-              }
-            ?>
-          </select>
-           - Año:
-          <select name="birth_year">
-            <?php
-              for($i = 2000; $i >= 1940; $i--){
-                echo "<option>$i</option>";
-              }
-            ?>
-          </select>
-          <h3>Ingrese su contraseña:</h3>
-          <input type="password" name="password">
-          <br>
-          <h3>Ingrese su contraseña nuevamente:</h3>
-          <input type="password" name="password_confirmation">
-          <br>
-          <br>
-          <input type="submit" text="Iniciar Sesión">
-        </form>
+      <div id="body">
+        <h1>Bienvenido a Aventón!</h1>
+          <div class="form_container">
+            <h2>Iniciar sesión</h2>
+            <form>
+              <h3>Ingrese su mail:</h3>
+              <input type="email" name="mail">
+              <br>
+              <h3>Ingrese su contraseña:</h3>
+              <input type="password" name="password">
+              <br><br>
+              <input type="submit" text="Iniciar Sesión">
+              <br><br>
+              <a href="/php/recuperar_contraseña.php">Olvide mi contraseña!</a>
+            </form>
+          </div>
+          <div class="form_container">
+            <h2>Crear una cuenta</h2>
+            <form action="/php/registrar_usuario.php" method="post">
+              <h3>Ingrese su nombre completo:</h3>
+              <input type="text" name="name">
+              <br>
+              <h3>Ingrese su mail:</h3>
+              <input type="email" name="mail">
+              <br>
+              <h3>Ingrese su fecha de nacimiento:</h3>
+              Día:
+              <select name="birth_day">
+                <?php
+                  for($i = 1; $i <= 31; $i++){
+                    echo "<option>$i</option>";
+                  }
+                  ?>
+              </select>
+               - Mes:
+              <select name="birth_month">
+                <?php
+                  for($i = 1; $i <= 12; $i++){
+                    echo "<option>$i</option>";
+                  }
+                ?>
+              </select>
+               - Año:
+              <select name="birth_year">
+                <?php
+                  for($i = 2000; $i >= 1940; $i--){
+                    echo "<option>$i</option>";
+                  }
+                ?>
+              </select>
+              <h3>Ingrese su contraseña:</h3>
+              <input type="password" name="password">
+              <br>
+              <h3>Ingrese su contraseña nuevamente:</h3>
+              <input type="password" name="password_confirmation">
+              <br>
+              <br>
+              <input type="submit" text="Iniciar Sesión">
+            </form>
+        </div>
       </div>
-    </div>
-    <div id="footer">
-      <a id="help_link"  href="/php/ayuda.php">Ayuda y Contacto</a>
-    </div>
+      <div id="footer">
+        <a id="help_link"  href="/php/ayuda.php">Ayuda y Contacto</a>
+      </div>
+  </div>
   </body>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
--- php/registrar_usuario.php
+++ php/registrar_usuario.php
@@ -1,3 +1,35 @@
 <?php
-  echo "Nombre: " . $_POST["mail"]
+  $my_sql_user = "root";
+  $my_sql_password = "";
+  $my_sql_server = "localhost";
+  $my_sql_db= "aventon";
+
+  $conn = mysqli_connect($my_sql_server, $my_sql_user, $my_sql_password, $my_sql_db);
+  if(!$conn){
+  	echo "No fue posible conectarse: ".mysql_error();
+  } else {
+  	echo "Conexion exitosa<br>";
+  }
+
+  $user_name = mysqli_real_escape_string($conn, $_POST['name']);
+  $user_mail = mysqli_real_escape_string($conn, $_POST['mail']);
+  $user_birth_day = mysqli_real_escape_string($conn, $_POST['birth_day']);
+  $user_birth_month = mysqli_real_escape_string($conn, $_POST['birth_month']);
+  $user_birth_year = mysqli_real_escape_string($conn, $_POST['birth_year']);
+  $user_password = mysqli_real_escape_string($conn, $_POST['password']);
+  $user_password_confirm = mysqli_real_escape_string($conn, $_POST['password_confirmation']);
+
+  $query = "SELECT * FROM usuario WHERE mail='$user_mail'";
+  $result = mysqli_query($conn, $query);
+
+  if(mysqli_fetch_assoc($result)){
+  	echo "Ingrese el mail otra vez<br>";
+  } else {
+  	echo "Registrando";
+  	$query = "INSERT INTO usuario (nombre, mail, contrasenia) VALUES('$user_name', '$user_mail', '$user_password')";
+  	mysqli_query($conn, $query);
+  	echo "registrado.";
+  }
+  
+  mysqli_close($conn);
 ?>
