904f51fc231980d3e190d66f2cafb38ab2c0ab1c
Joaquín de Antueno <joaquindea@hotmail.es>
2018-05-30 20:33:05 -0300

    Arreglos menores

--- js/listar_vehiculos.js
+++ js/listar_vehiculos.js
@@ -1,6 +1,3 @@
-/*$(".delete_vehicle").click(function(e){
-  e.preventDefault();
-  if(window.confirm("¿Esta seguro de que desea eliminar el vehículo?")){
-    $.redirectPost("/php/baja_vehiculo.php", {plate: $(this).siblings('input').val()});
-  }
-});*/
+$(".delete_vehicle").click(function(e){
+  return window.confirm("¿Esta seguro de que desea eliminar el vehículo?");
+});
