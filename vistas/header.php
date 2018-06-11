b372cdd4441e60335aab745b241109930d157fe2
Joaquín de Antueno <joaquindea@hotmail.es>
2018-06-11 16:45:43 -0300

    Ver Viajes listo

--- vistas/header.php
+++ vistas/header.php
@@ -6,7 +6,7 @@
   </form>
   <button class="btn"><a href="/php/cerrar_sesion.php">Cerrar Sesión</a></button>
   <button class="btn"><a href="/vistas/listar_vehiculos.php">Mis Vehículos</a></button>
-  <button class="btn"><a href="#">Mis Viajes</a></button>
+  <button class="btn"><a href="/vistas/ver_viajes.php">Mis Viajes</a></button>
   <button class="btn"><a href="/vistas/ver_perfil.php">Mi Perfil</a></button>
   <button class="btn" id="hello-user-css">Bienvenido <?php echo  $_SESSION["user_name"] ?></button>
 </div>
---
+++ vistas/ver_viajes.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css" >
+    <link href="/css/index.css" rel="stylesheet" type="text/css">
+    <link href="/css/listar_vehiculos.css" rel="stylesheet" type="text/css">
+  </head>
+  <body> 
+    <?php
+      include("../php/verficar_sesion.php");
+      include("../php/abrir_conexion.php");
+      include("../php/utils.php");
+      include("header.php");
+    ?>
+    <div id="body"> 
+      <h1>Mis viajes</h1>
+      <?php
+        $id = $_SESSION["user_id"];
+        $sql = "SELECT * FROM viajes WHERE id_usuario='$id'";
+        $result = mysqli_query($conn, $sql);
+        if($result){
+          if($trip = mysqli_fetch_assoc($result)){
+            while($trip){?>
+          
+      <div class="vehicle">
+        <div class="vehicle-info">
+          <h3><?= $trip["origen"] ?> --> <?= $trip["destino"] ?></h3>
+          <?= date("d/m/Y H:i", strtotime($trip["fecha_hora"])); ?> - Duración:
+          <?php
+            $hours = floor($trip["duracion"]);
+            $minutes = $trip["duracion"]-$hours;
+            if($hours > 0){
+              echo $hours.(($hours>=2)?" horas":" hora");
+              if($minutes > 0){
+                echo " y";
+              }
+            }
+            if($minutes > 0){
+              echo " ".($minutes*60)." minutos";
+            }
+            echo ".";
+          ?>
+	  <br>
+          <br>
+	  <form class="" action="/vistas/editar_viaje.php" method="post">
+            <input type="hidden" name="trip_id" value="<?= $trip["id_viaje"]; ?>">
+            <button class="btn" name="">Modificar Viaje</button>
+          </form>
+	  <form class="" action="/php/baja_viaje.php" method="post">
+            <input type="hidden" name="trip_id" value="<?= $trip["id_viaje"]; ?>">
+            <button class="btn btn-danger delete_trip" name="">Eliminar</button>
+          </form>
+        </div>
+      </div>
+
+      <?php
+              $trip = mysqli_fetch_assoc($result);
+            }
+          } else {
+            echo "<h2>No hay ningun viaje registrado!</h2><br>";
+          }
+        } else {
+          echo "Hubo un error al conectarse con la base de datos. <br> Por favor, intentelo nuevamente mas tarde.";
+        }
+      ?>
+      <button class="btn btn-success"><a href="/vistas/registrar_viaje.php">Crear Viaje</a></button>
+    </div>
+    <?php
+      include("footer.php");
+    ?>
+  </body>
+  <?php 
+    include("bootstrap.php"); 
+    include("../php/cerrar_conexion.php");
+  ?>
+  <script src="/js/registrar_usuario.js"></script>
+</html>
