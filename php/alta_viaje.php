944c1242cc57575da97382c89534e01e41d1807b
Joaquín de Antueno <joaquindea@hotmail.es>
2018-06-14 16:32:20 -0300

    Crear viajes terminado (ahora si)

--- php/alta_viaje.php
+++ php/alta_viaje.php
@@ -36,11 +36,22 @@
     while($request = mysqli_fetch_assoc($result)){
       $query = "SELECT * FROM viajes WHERE id_viaje='".$request["id_viaje"]."'";
       $trip = mysqli_fetch_assoc(mysqli_query($conn, $query));
+      echo "Viaje: "; print_r($trip);
       if(!checkTripDates($datetime, $duration, $trip["fecha_hora"], $trip["duracion"])){
         header("Location: /vistas/ver_viajes.php?date_error");
         exit;
       }
     }
+  } else {
+    header("Location: /vistas/ver_viajes.php?db_error");
+  }
+
+  $query = "INSERT INTO viajes (id_usuario, patente, origen, destino, duracion, fecha_hora, descripcion, tipo, costo, tarjeta)
+      VALUES ($user_id, '$plate', '$origin', '$destination', $duration, '$datetime', '$desc', '$type', $price, $card)";
+  $result = mysqli_query($conn, $query);
+
+  if($result){
+    header("Location: /vistas/ver_viajes.php?reg_success");
   } else {
     header("Location: /vistas/ver_viajes.php?db_error");
   }
