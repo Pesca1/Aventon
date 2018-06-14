5d742ccace95133b5765b72c52e139a302ea1314
Sebastian Zecenarro <sebazecenarro@gmail.com>
2018-06-14 15:19:23 -0300

    vista inical editar viaje

---
+++ vistas/editar_viaje.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css" >
+    <link href="/css/index.css" rel="stylesheet" type="text/css">
+    <link href="/css/listar_vehiculos.css" rel="stylesheet" type="text/css">
+  </head>
+  <body> 
+    <?php
+      include("../php/verficar_sesion.php");
+      include("../php/abrir_conexion.php");
+      include("../php/utils.php");
+      include("header.php");
+    ?>
+    
+
+
+  </body>
+
+    </html>
