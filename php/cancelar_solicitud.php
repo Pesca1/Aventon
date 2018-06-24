eb96cddea4e1767a97e57805a69ad6d22be3501c
roby870 <33817831+roby870@users.noreply.github.com>
2018-06-24 15:47:49 -0300

    Update cancelar_solicitud.php
    notifica por mail

--- php/cancelar_solicitud.php
+++ php/cancelar_solicitud.php
@@ -20,13 +20,34 @@
 
 	  $request=$_POST['request_id'];
 
-    
+
+    $query0 = "SELECT * FROM solicitud WHERE id_solicitud='$request'";
+    $result0 = mysqli_query($conn, $query0);
+    $id_pasajero = mysqli_fetch_assoc($result0);
+    $id_p=$id_pasajero['id_pasajero'];
+    $query0 = "SELECT * FROM usuario WHERE id_usuario='$id_p'";
+    $result0 = mysqli_query($conn, $query0);
+    $mail_pasajero=mysqli_fetch_assoc($result0);
+    $mail_p=$mail_pasajero['mail'];
+    $query0 = "SELECT * FROM solicitud WHERE id_solicitud='$request'";
+    $result0 = mysqli_query($conn, $query0);
+    $id_viaje = mysqli_fetch_assoc($result0);
+    $id_v=$id_viaje['id_viaje'];
+    $query = "SELECT * FROM viajes WHERE id_viaje='$id_v'";
+    $result = mysqli_query($conn, $query);
+    $trip = mysqli_fetch_assoc($result);
     
     $query = "DELETE FROM solicitud WHERE id_solicitud='$request'";
     $result = mysqli_query($conn, $query);
     
     if($result){
       header("Location: /vistas/ver_solicitudes.php?deleted_success");
+
+      $sent = mail("$mail_p", "¡El viaje al que se ha postulado fue cancelado! Aventon", "Hola! Nos comunicamos para informarte  el viaje: \n ".$trip['origen']." con destino a ".$trip['destino']." con fecha de salida ".$trip['fecha_hora']." \n fue cancelado. \n Equipo de Aventon ");
+        if(!$sent){
+          header("Location: /vistas/ver_solicitudes.php?notification_error_cancel");
+        }
+
     } else {
       header("Location: /vistas/ver_solicitudes.php?deleted_error");
     }
