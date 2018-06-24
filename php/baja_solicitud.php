24c16986b0a5472f009fcac3472486ad19037ddc
roby870 <33817831+roby870@users.noreply.github.com>
2018-06-24 15:55:40 -0300

    Create baja_solicitud.php

---
+++ php/baja_solicitud.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Aventón</title>
+    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css" >
+    <link href="/css/index.css" rel="stylesheet" type="text/css">
+    <link href="/css/listar_vehiculos.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <?php
+      include("verficar_sesion.php");
+      include("abrir_conexion.php");
+      include("utils.php");
+      include("../vistas/header.php");
+    ?>
+    
+    <div id="body">
+
+    <?php
+
+    $request=$_POST['request_id'];
+
+    $query0 = "SELECT * FROM solicitud WHERE id_solicitud='$request'";
+    $result0 = mysqli_query($conn, $query0);
+    $id_pasajero = mysqli_fetch_assoc($result0);
+    $id_p=$id_pasajero['id_pasajero'];
+    $query0 = "SELECT * FROM usuario WHERE id_usuario='$id_p'";
+    $result0 = mysqli_query($conn, $query0);
+    $mail_pasajero=mysqli_fetch_assoc($result0);
+    $mail_p=$mail_pasajero['mail'];
+    $query0 = "SELECT * FROM solicitud WHERE id_solicitud='$request'";
+    $result0 = mysqli_query($conn, $query0);
+    $id_viaje = mysqli_fetch_assoc($result0);
+    $id_v=$id_viaje['id_viaje'];
+    $query = "SELECT * FROM viajes WHERE id_viaje='$id_v'";
+    $result = mysqli_query($conn, $query);
+    $trip = mysqli_fetch_assoc($result);
+    
+    $query = "DELETE FROM solicitud WHERE id_solicitud='$request'";
+    $result = mysqli_query($conn, $query);
+    
+    if($result){
+      header("Location: /vistas/ver_solicitudes.php?deleted_success");
+
+      $sent = mail("$mail_p", "¡El viaje al que se ha postulado fue cancelado! Aventon", "Hola! Nos comunicamos para informarte  el viaje: \n ".$trip['origen']." con destino a ".$trip['destino']." con fecha de salida ".$trip['fecha_hora']." \n fue cancelado. \n Equipo de Aventon ");
+      if(!$sent){
+          header("Location: /vistas/ver_solicitudes.php?notification_error_cancel");
+        }
+
+    } else {
+      header("Location: /vistas/ver_solicitudes.php?deleted_error");
+    }
+
+    ?>
+
+    </div>
+
+    <?php
+      include("vistas/footer.php");
+    ?>
+  </body>
+  <?php 
+    include("vistas/bootstrap.php"); 
+    include("php/cerrar_conexion.php");
+  ?>
+  <script src="/js/registrar_usuario.js"></script>
+
+</html>
+
