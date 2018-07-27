cf4cf15032c4893afd966a93fd6aae6436e209c2
roby870 <33817831+roby870@users.noreply.github.com>
2018-07-27 13:47:37 -0300

    Create calificar_pasajero.php

---
+++ vistas/calificar_pasajero.php
@@ -0,0 +1,88 @@
+<?php
+  include("../php/verficar_sesion.php");
+  include("../php/abrir_conexion.php");
+  include("../php/utils.php");
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Aventón</title>
+    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css" >
+    <link href="/css/index.css" rel="stylesheet" type="text/css">
+    <link href="/css/listar_vehiculos.css" rel="stylesheet" type="text/css">
+    <link href="/css/listar_solicitudes.css" rel="stylesheet" type="text/css">
+</head>
+  <body>
+    <?php include("header.php"); ?>
+    <div id="body">
+  		
+    	
+	<?php
+
+    	 $calification_id=$_POST['calification_id'];
+
+    	 $query = "SELECT * FROM puntua_pasajero WHERE id_puntua_pasajero='".$calification_id."'";
+         $result = mysqli_query($conn, $query);
+         $calification = mysqli_fetch_assoc($result);
+
+    	 $trip = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM viajes WHERE id_viaje='".$calification["id_viaje"]."'"));
+
+         $passenger= mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM usuario WHERE id_usuario='".$calification["id_pasajero"]."'"));
+    ?>
+
+
+     <h3>Calificar al pasajero <?php echo $passenger["nombre"]." ".$passenger["apellido"]  ?> </h3>
+   	 <br>
+   	 <h3>Fecha y hora del viaje: <?php echo $trip["fecha_hora"]  ?> </h3>
+   	 <br>
+   	 <h3> <?php echo "Desde ".$trip["origen"]." hasta ".$trip["destino"] ?> </h3>
+   	 <br>
+   	 <br>
+     
+    <div class="container">
+        
+            <form method="POST" action="/php/puntuar_pasajero.php" enctype="multipart/form-data" autocomplete="off">
+               <div class="form-group col-md-8">
+                  <div class="row">
+                    
+                      <label for="inputComment">Comentario:</label>
+                      <input type="text" name="comentario" class="form-control" id="comentario" value="">
+                    
+                  </div>
+                </div>
+              <br>
+              <div class="row">
+                
+                <div class="col-md-5">
+                        
+                        <input type="radio"name="valor" value="Positivo"> Calificación positiva <br> <br>
+       
+                        <input type="radio"name="valor" value="Neutro" checked>  Calificación neutra <br> <br> 
+
+                        <input type="radio"name="valor" value="Negativo">  Calificación negativa <br> <br> <br> <br>
+                  
+                  <input type="hidden" name="passenger" value= "<?php echo $passenger["id_usuario"] ?>"> 
+                  <input type="hidden" name="calification" value= "<?php echo $calification_id ?>">
+                  <button type="submit" class="action btn btn-danger delete_card">Calificar</button>
+                </div>
+              </div>
+            </form>
+
+      </div>
+      </div>
+      
+
+ <?php
+      include("footer.php");
+    ?>
+  </body>
+  <?php 
+    include("bootstrap.php"); 
+    include("../php/cerrar_conexion.php");
+  ?>
+  <script src="/js/registrar_usuario.js"></script>
+  <script src="/js/ver_solicitudes.js"></script>
+
+
+</html>
